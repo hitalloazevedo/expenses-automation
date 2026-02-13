e1533f2c81c8884d2abeb0c4bf8d540f314040b3
Hitallo Azevedo <azevedohitallo@gmail.com>
2026-02-12 21:38:28 -0300

    refactor: update implementation to use env var instead of json

--- src/sheets.js
+++ src/sheets.js
@@ -1,7 +1,7 @@
 import { google } from 'googleapis';
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: './service-account.json',
+  credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS),
   scopes: ['https://www.googleapis.com/auth/spreadsheets']
 });
 
